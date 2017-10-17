203df7ed3729a1cf17849fb58a68b5e1a7e8c76e
Rene Barto <rene.barto66@gmail.com>
2017-10-17 13:18:42 +0200

    Added support for I2S, I2C, SPI and Hifiberry settings.

--- board/raspberrypi/post-image.sh
+++ board/raspberrypi/post-image.sh
@@ -18,6 +18,46 @@ dtoverlay=pi3-miniuart-bt
 __EOF__
 		fi
 		;;
+		--enable-audio)
+		if ! grep -qE '^dtparam=audio=on' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
+			echo "Adding 'dtparam=audio=on' to config.txt (enables audio)."
+			cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
+
+# enables audio
+dtparam=audio=on
+__EOF__
+		fi
+		;;
+		--enable-i2s)
+		if ! grep -qE '^dtparam=i2s=on' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
+			echo "Adding 'dtparam=i2s=on' to config.txt (enables I2S)."
+			cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
+
+# enables I2S
+dtparam=i2s=on
+__EOF__
+		fi
+		;;
+		--enable-i2c)
+		if ! grep -qE '^dtparam=i2c=on' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
+			echo "Adding 'dtparam=i2c=on' to config.txt (enables I2C)."
+			cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
+
+# enables I2C
+dtparam=i2c=on
+__EOF__
+		fi
+		;;
+		--enable-spi)
+		if ! grep -qE '^dtparam=spi=on' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
+			echo "Adding 'dtparam=spi=on' to config.txt (enables SPI)."
+			cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
+
+# enables SPI
+dtparam=spi=on
+__EOF__
+		fi
+		;;
 		--aarch64)
 		# Run a 64bits kernel (armv8)
 		sed -e '/^kernel=/s,=.*,=Image,' -i "${BINARIES_DIR}/rpi-firmware/config.txt"
